908a647e81957e4b563d9ae577ca8becdbfa9c64
Dan Oswalt <do189705@gmail.com>
2018-06-17 20:06:58 -0700

    list cards in home

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <nav>
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">ListWar</router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/signup">Sign-Up</router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+  .nav-wrapper .brand-logo {
+  }
+</style>
