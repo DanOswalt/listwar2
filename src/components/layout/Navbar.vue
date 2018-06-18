eca9ac65158544577b74c7c3c26fe113460976ba
maclaptop <maclaptop@c991228.local>
2018-06-17 21:12:46 -0700

    thumbnail components

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <nav>
-    <div class="nav-wrapper">
+    <div class="nav-wrapper blue-grey darken-2">
       <router-link to="/" class="brand-logo">ListWar</router-link>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><router-link to="/login">Login</router-link></li>
@@ -20,6 +20,4 @@ export default {
 </script>
 
 <style lang="css">
-  .nav-wrapper .brand-logo {
-  }
 </style>
---
+++ src/components/views/Home/ListThumbnail.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="thumbnail">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title center">{{ list.title }}</span>
+        <ul class="list-entries collection">
+          <li class="list-entry collection-item blue-grey-text darken-2" v-for="(entry, index) in list.entries" :key="index">{{ entry.value }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListThumbnail',
+  props: [
+    'list'
+  ],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+</style>
