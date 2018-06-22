eb51a0e6121a420dc4ad5520b3e74bf23aa0f85f
maclaptop <maclaptop@c991228.local>
2018-06-22 09:25:49 -0700

    createlist button and vue started

---
+++ src/components/views/CreateList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="createlist container">
+
+  </div>
+</template>
+
+<script>
+// import savedLists from '@/mock/savedLists.json'
+
+export default {
+  name: 'CreateList',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
