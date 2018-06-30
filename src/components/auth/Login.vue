1b2a09cd84d1113db3786668aa9ee57c2444a635
maclaptop <maclaptop@c991228.ptp>
2018-06-29 21:38:29 -0700

    auth unstylish but operational

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div class="login container">
+    <form @submit.prevent="login" class="card-panel teal-text text-darken-4">
+      <h4 class="card-title center">Log In</h4>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div class="field center">
+        <button type="submit" class="btn teal">Log Me In</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+// import db from '@/firebase/init'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: null,
+      password: null,
+      msg: null
+    }
+  },
+  methods: {
+    login () {
+      if (this.email && this.password) {
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then(cred => {
+            console.log('user:', cred.user)
+            this.$router.push({ name: 'Home'})
+          })
+          .catch(err => {
+            console.log('error', err.message)
+          })
+
+        this.msg = null
+      } else {
+        this.msg = 'Please fill out both fields'
+        console.log(this.msg)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -3,18 +3,29 @@
     <div class="nav-wrapper orange darken-4">
       <router-link to="/" class="brand-logo">ListWar</router-link>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><router-link to="/login">Login</router-link></li>
-        <li><router-link to="/register">Sign Up</router-link></li>
+        <li><router-link :to="{ name: 'Register' }">Sign Up</router-link></li>
+        <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
+        <li @click="logout"><a>Logout</a></li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'Navbar',
   data () {
     return {}
+  },
+  methods: {
+    logout () {
+      firebase.auth().signOut()
+        .then(() => {
+          this.$router.push({ name: 'Home' })
+        })
+    }
   }
 }
 </script>
